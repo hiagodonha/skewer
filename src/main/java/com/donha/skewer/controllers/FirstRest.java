f47c1ed4d99db7230d58cc769c8a5c7732ff6da1
hiago <hiagodefenix@gmail.com>
2024-02-12 20:01:34 -0300

    refact: rename classes.

--- src/main/java/com/donha/skewer/controllers/FirstRest.java
+++ src/main/java/com/donha/skewer/controllers/FirstRest.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class TesteRest {
+public class FirstRest {
 
     @GetMapping
     public String teste() {
