f67f2a9c98c52ea1310282f1a08bc121b5c0e373
hiago <hiagodefenix@gmail.com>
2024-02-14 16:36:53 -0300

    feat: update

--- src/main/java/com/donha/skewer/dao/FirstDao.java
+++
@@ -1,9 +0,0 @@
-package com.donha.skewer.dao;
-
-import com.donha.skewer.models.First;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FirstDao extends JpaRepository<First, Long> {
-}
