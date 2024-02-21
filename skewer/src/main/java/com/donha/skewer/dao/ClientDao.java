8dd90cf763e7b3fec0b47afdbee2e372ad8a954a
hiago <hiagodefenix@gmail.com>
2024-02-21 15:56:03 -0300

    Change First to Client in ClientDao interface.

--- skewer/src/main/java/com/donha/skewer/dao/ClientDao.java
+++ skewer/src/main/java/com/donha/skewer/dao/ClientDao.java
@@ -1,7 +1,7 @@
 package com.donha.skewer.dao;
 
-import com.donha.skewer.models.First;
+import com.donha.skewer.models.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClientDao extends JpaRepository<First, Long> {
+public interface ClientDao extends JpaRepository<Client, Long> {
 }
